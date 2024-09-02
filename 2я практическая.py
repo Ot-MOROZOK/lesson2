55e574907b00808d6720195aa3920296eacd8307
Ot-MOROZOK <ueptsfu@gmail.com>
2024-09-02 14:23:03 +0700

    Add files via upload

---
+++ 2я практическая.py
@@ -0,0 +1,5 @@
+completed_homework = '12'
+hours_spent = '1.5'
+well = 'Phython'
+time_per_task = float(hours_spent) / int(completed_homework)
+print('Курс:',well+',','всего задач:'+completed_homework+',','затрачено часов:'+hours_spent+',','среднее время выполнения',time_per_task, 'часа.')
